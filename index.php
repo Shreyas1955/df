38a27a8e59ff6f4b1c8ed242657a2132cea471fd
Shreyas1955 <71566050+Shreyas1955@users.noreply.github.com>
2020-11-28 20:10:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,26 @@
 <?php
 $title = "Home";
-$content = '<img src="Images/coffee1.png" class="imgLeft" />
+$content = '<img src="coffee1.png" class="imgLeft" />
 <h3>Title 1</h3>
 <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lectus urna,
-    viverra in luctus quis, ullamcorper quis lorem. Vestibulum vulputate pellentesque
-    velit, et placerat dolor pulvinar in. Class aptent taciti sociosqu ad litora torquent
-    per conubia nostra, per inceptos himenaeos. Sed sit amet velit at purus elementum
+    PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group. Sed sit amet velit at purus elementum
     dapibus. Nulla dapibus auctor vulputate. Sed cursus nisi at mauris mollis semper.
     Vestibulum consectetur cursus dui sit amet pretium.
 </p>
 
-<img src="Images/coffee2.png" class="imgRight"/>
+<img src="coffee2.png" class="imgRight"/>
 <h3>Title 2</h3>
 <p>
-    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-    himenaeos. Donec porttitor placerat nunc a consectetur. Ut fermentum eleifend molestie.
-    Donec fermentum risus sit amet ante cursus cursus. In hac habitasse platea dictumst.
-    Praesent semper ante ut felis molestie aliquet. Nam quis dui a magna molestie blandit
-    id et justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-    per inceptos himenaeos. Sed felis mauris, mattis a vulputate a, gravida nec metus.
-    Vivamus elit augue, ullamcorper eget scelerisque vitae, tincidunt eget lorem. Curabitur
+    PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.ue vitae, tincidunt eget lorem. Curabitur
     id nibh libero, sed pulvinar nisi. Curabitur ultrices, neque in dignissim viverra,
     justo nisl dignissim magna, a tempor eros turpis in diam. Suspendisse potenti. Sed
     tincidunt est ac elit bibendum nec varius ante vestibulum.
 </p>
 
-<img src="Images/coffee3.png" class="imgLeft" />
+<img src="coffee3.png" class="imgLeft" />
 <h3>Title 3</h3>
 <p>
-    In hac habitasse platea dictumst. In purus leo, consequat nec porta a, varius sed
-    dui. Nulla placerat mollis tincidunt. Praesent at elit mi, a posuere quam. Suspendisse
-    vel leo eu orci lacinia vestibulum. Fusce vel sem fermentum lectus tempus ultrices.
+  PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.
     Morbi arcu sem, rhoncus sit amet rutrum eget, interdum sit amet leo. Ut mattis lorem
     vitae dolor tincidunt mattis a lacinia velit. Cras sed nibh at urna imperdiet laoreet
     quis a risus.
